925e4fa653a2a47b683ef82e1c180c8da39ecac6
JoshuaLeeOverton <joshua.leeoverton@gmail.com>
2025-06-16 17:58:38 +0200

    add a rudimentary edit state to get the full crud operations working

--- modules/src/components/todo-item/todo-item.js
+++ modules/src/components/todo-item/todo-item.js
@@ -1,5 +1,23 @@
 const template = document.createElement("template")
 
+const buttonInitialState = `
+  <button id="todo-actions-edit">
+    Edit
+  </button>
+  <button id="todo-actions-delete">
+    Delete
+  </button>
+`
+
+const buttonEditState = `
+  <button id="todo-actions-save">
+    Save
+  </button>
+  <button id="todo-actions-cancel">
+    Cancel
+  </button>
+`
+
 template.innerHTML = `
   <style>
     #todo-container {
@@ -10,21 +28,24 @@ template.innerHTML = `
       justify-content: space-between;
     }
 
+    #todo-info-container {
+      display: flex;
+    }
+
     #todo-date {
       padding-right: 10px;
       margin-left: 25px;
     }
   </style>
   <div id="todo-container">
-    <span id="todo-title"></span>
-    <div id="todo-actions">
+    <div id="todo-input-container"> 
+      <span id="todo-title"></span>
+    </div>
+    <div id="todo-info-container">
       <span id="todo-date"></span>
-      <button id="todo-actions-edit">
-        Edit
-      </button>
-      <button id="todo-actions-delete">
-        Delete
-      </button>
+      <div id="todo-actions">
+        ${buttonInitialState}
+      </div>
     </div>
   </div>
 `
@@ -37,18 +58,19 @@ export default class TodoItem extends HTMLElement {
     shadow.append(template.content.cloneNode(true))
 
     this.todoTitle = shadow.getElementById("todo-title")
-
     this.todoDate = shadow.getElementById("todo-date")
-
     this.todoEditButton = shadow.getElementById("todo-actions-edit")
     this.todoDeleteButton = shadow.getElementById("todo-actions-delete")
+    this.todoActions = shadow.getElementById("todo-actions")
+    this._isEditMode = false
+    this._currentTitle = ""
 
     this._externalOnClickHandler = null
   }
 
   connectedCallback() {
     // Store bound handlers for proper removal
-    this._handleEditClick = () => this._handleButtonClick("edit")
+    this._handleEditClick = () => this._handleEditButtonClick()
     this._handleDeleteClick = () => this._handleButtonClick("delete")
     this.todoEditButton.addEventListener("click", this._handleEditClick)
     this.todoDeleteButton.addEventListener("click", this._handleDeleteClick)
@@ -58,11 +80,64 @@ export default class TodoItem extends HTMLElement {
     e.stopPropagation()
   }
 
-  _handleButtonClick(action) {
+  _handleEditButtonClick() {
+    this._setEditState(true)
+  }
+
+  _setEditState(isEdit) {
+    this.todoActions.innerHTML = isEdit ? buttonEditState : buttonInitialState
+    this._isEditMode = isEdit
+
+    // Swap title span with input in edit mode
+    if (isEdit) {
+      this._currentTitle = this.todoTitle.textContent
+      const input = document.createElement("input")
+      input.type = "text"
+      input.value = this._currentTitle
+      input.id = "todo-title-input"
+      this.todoTitle.replaceWith(input)
+      this.todoTitleInput = input
+    } else {
+      // Swap input back to span
+      if (this.todoTitleInput) {
+        const span = document.createElement("span")
+        span.id = "todo-title"
+        span.textContent = this.todoTitleInput.value
+        this.todoTitleInput.replaceWith(span)
+        this.todoTitle = span
+      }
+    }
+
+    if (isEdit) {
+      const todoSaveButton = this.shadowRoot.getElementById("todo-actions-save")
+      const todoCancelButton = this.shadowRoot.getElementById(
+        "todo-actions-cancel"
+      )
+      todoSaveButton.addEventListener("click", () => {
+        this._setEditState(false)
+        this._handleButtonClick("update", {
+          title: this.todoTitleInput.value,
+          date: this.todoDate.innerText
+        })
+      })
+      todoCancelButton.addEventListener("click", () =>
+        this._setEditState(false)
+      )
+    } else {
+      const todoEditButton = this.shadowRoot.getElementById("todo-actions-edit")
+      const todoDeleteButton = this.shadowRoot.getElementById(
+        "todo-actions-delete"
+      )
+      todoEditButton.addEventListener("click", this._handleEditClick)
+      todoDeleteButton.addEventListener("click", this._handleDeleteClick)
+    }
+  }
+
+  _handleButtonClick(action, payload = {}) {
     this.dispatchEvent(
       new CustomEvent("todo-click", {
         // Changed from "onClick" to "itemAction" for clarity
-        detail: { action, id: this.id }, // This will be "edit" or "delete"
+        detail: { action, id: this.id, payload }, // This will be "edit" or "delete"
         bubbles: true,
         composed: true
       })
